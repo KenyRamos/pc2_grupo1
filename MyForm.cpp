4a31d35ab37ca3a2ac2330141f7d3439930f4182
Keny <keny.ramosp@gmail.com>
2020-11-07 08:55:55 -0500

    agregando windows form

---
+++ MyForm.cpp
@@ -0,0 +1,11 @@
+#include "MyForm.h"
+using namespace System;
+using namespace System::Windows::Forms;
+
+[STAThread]
+void main()
+{
+	Application::EnableVisualStyles();
+	Application::SetCompatibleTextRenderingDefault(false);
+	Application::Run(gcnew 	Project3::MyForm()); //Project 1 es el nombre del proyecto
+}
