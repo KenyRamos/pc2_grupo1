4dcdf8da0cb19df538d79edb2c08f0ce212c4cf4
gilma23 <gilma.j.baca@gmail.com>
2020-11-08 14:22:20 -0500

    mover_monigote

--- Monigote.cpp
+++ Monigote.cpp
@@ -14,3 +14,10 @@ void Monigote::Dibujar(Graphics^ g, Bitmap^ bmp)
 	Rectangle porcion = Rectangle(c * w, f * h, w, h);
 	g->DrawImage(bmp, Area(), porcion, GraphicsUnit::Pixel);
 }
+void Monigote::Mover_Monigote(Graphics^ g)
+{
+	if (x + dx >= 0 && x + ancho + dx < g->VisibleClipBounds.Width)
+		x += dx;
+	if (y + dy >= 0 && y + alto + dy < g->VisibleClipBounds.Height)
+		y += dy;
+}
--- Monigote.h
+++ Monigote.h
@@ -7,6 +7,7 @@ public:
 	Monigote();
 	Monigote(int px, int py, Bitmap^ bmp);
 	void Dibujar(Graphics^ g, Bitmap^ bmp);
+	void Mover_Monigote(Graphics^ g);
 	int retornar_ancho() { return w; }
 	int retornar_alto() { return h; }
 };
